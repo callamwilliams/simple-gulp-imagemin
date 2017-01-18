6f4a79c263ecf16831d3263cd6a19dd48cf4e0ac
Callam Williams <lhocdesign@gmail.com>
2017-01-18 14:36:43 +0000

    [cleanup] - tidied files

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,21 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var plumber = require('gulp-plumber');
 var changed = require('gulp-changed');
-var imgSrc = 'images/**/*',
-	imgDst = 'dist/';
+var source = 'images/**/*';
+var	destination = 'dist/';
+
 var onError = function(err) {
 	console.log(err);
 }
 
 gulp.task('default', () =>
-	gulp.src(imgSrc)
+	gulp.src(source)
 	.pipe(plumber({
 		errorHandler: onError
 	}))
-	.pipe(changed(imgDst))
+	.pipe(changed(destination))
 	.pipe(imagemin({
 		progressive: true
 	}))
-	.pipe(gulp.dest(imgDst))
+	.pipe(gulp.dest(destination))
 );
